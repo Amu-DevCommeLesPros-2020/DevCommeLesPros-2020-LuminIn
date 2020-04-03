92107b4f1f46f6ed24df50bb36b3e07cd2527579
Thierry Seegers <surfintheusa@yahoo.com>
2020-04-03 16:47:04 +0200

    Ajout tests ouverture et fermeture de BD.

--- test/main.c
+++ test/main.c
@@ -1,3 +1,4 @@
+#include "bd.h"
 #include "journal.h"
 
 #include <stdio.h>
@@ -46,6 +47,31 @@ int main()
 
         j_fermer();
         TEST(access(nom_journal, F_OK) == 0);
+    }
+
+    // Tests pour la création d'une BD.
+    {
+#define chemin_db "./db-creation-test"
+#define nom_table_compagnie "compagnie.csv"
+#define nom_table_poste "poste.csv"
+#define nom_table_employe "employe.csv"
+#define nom_table_chercheur "chercheur.csv"
+        
+        remove(chemin_db);
+
+        bd_ouvrir(chemin_db);
+        TEST(access(chemin_db "/" nom_table_compagnie, F_OK) == 0);
+        TEST(access(chemin_db "/" nom_table_poste, F_OK) == 0);
+        TEST(access(chemin_db "/" nom_table_employe, F_OK) == 0);
+        TEST(access(chemin_db "/" nom_table_chercheur, F_OK) == 0);
+
+        bd_fermer();
+        TEST(access(chemin_db "/" nom_table_compagnie, F_OK) == 0);
+        TEST(access(chemin_db "/" nom_table_poste, F_OK) == 0);
+        TEST(access(chemin_db "/" nom_table_employe, F_OK) == 0);
+        TEST(access(chemin_db "/" nom_table_chercheur, F_OK) == 0);
+
+        remove(chemin_db);
     }
 
     printf("%d/%d\n", tests_reussis, tests_executes);
