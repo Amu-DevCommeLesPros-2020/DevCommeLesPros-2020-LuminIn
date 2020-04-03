88f6be5dbf40fd74b4a56ed12b6bf41b35ca8d37
Thierry Seegers <surfintheusa@yahoo.com>
2020-04-03 18:02:09 +0200

    Introduction liste.

---
+++ lib/liste.c
@@ -0,0 +1,153 @@
+#include "liste.h"
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+node* l_make_node(void* data)
+{
+    node *n = malloc(sizeof(node));
+    n->data = data;
+    n->next = NULL;
+    n->previous = NULL;
+
+    return n;
+}
+
+void l_free_node(node* n)
+{
+    if(n) free(n->data);
+    free(n);
+
+    return;
+}
+
+void l_free_list(node* n)
+{
+    for(node *next = n; next;)
+    {
+        n = next;
+        next = n->next;
+        l_free_node(n);
+    }
+
+    return;
+}
+
+int l_length(node const* n)
+{
+    int i = n ? 1 : 0;
+    for(node const* p = n; p && p->next; p = p->next) ++i;
+    for(node const* p = n; p && p->previous; p = p->previous) ++i;
+
+    return i;
+}
+
+node* l_head(node* n)
+{
+    for(; n && n->previous; n = n->previous) {}
+
+    return n;
+}
+
+node* l_tail(node* n)
+{
+    for(; n && n->next; n = n->next) {}
+
+    return n;
+}
+
+node* l_skip(node* n, int i)
+{
+    if(i < 0)
+    {
+        for(; i && n && n->previous; n = n->previous)
+        {
+             ++i;
+        }
+    }
+    else if(i > 0)
+    {
+        for(; i && n && n->next; n = n->next)
+        {
+             --i;
+        }
+    }
+
+    return i ? NULL : n;
+}
+
+node* l_append(node** p, node* tail)
+{
+    assert(p);
+    if(!*p)
+    {
+        *p = tail;
+    }
+    else
+    {
+        node *last = l_tail(*p);
+        last->next = tail;
+        tail->previous = last;
+    }
+
+    return tail;
+}
+
+node* l_prepend(node** p, node* head)
+{
+    assert(p);
+    if(!*p)
+    {
+        *p = head;
+    }
+    else
+    {
+        node *first = l_head(*p);
+        first->previous = head;
+        head->next = first;
+    }
+
+    return head;
+}
+
+node* l_insert(node** p, node* body)
+{
+    assert(p);
+    if(!*p)
+    {
+        *p = body;
+    }
+    else
+    {
+        body->previous = (*p);
+        body->next = (*p)->next;
+
+        (*p)->next = body;
+        if(body->next)
+        {
+            body->next->previous = body;
+        }
+    }
+
+    return body;
+}
+
+node* l_remove(node* n)
+{
+    node *next = n ? n->next : NULL;
+
+    if(n && n->previous)
+    {
+        n->previous->next = n->next;
+    }
+
+    if(n && n->next)
+    {
+        n->next->previous = n->previous;
+    }
+
+    l_free_node(n);
+    
+    return next;
+}
--- makefile
+++ makefile
@@ -8,10 +8,11 @@ clean:
 build:
 	mkdir -p build
 
-liblumi: lib/bd.c lib/bd.h lib/journal.c lib/journal.h | build
+liblumi: lib/bd.c lib/bd.h lib/journal.c lib/journal.h lib/liste.h lib/liste.c | build
 	gcc -Wall -Werror -pedantic -g -c lib/bd.c -o build/bd.o
+	gcc -Wall -Werror -pedantic -g -c lib/liste.c -o build/liste.o
 	gcc -Wall -Werror -pedantic -g -c lib/journal.c -o build/journal.o
-	ar crs build/liblumi.a build/bd.o build/journal.o
+	ar crs build/liblumi.a build/bd.o build/liste.o build/journal.o
 
 luminin: | build
 
